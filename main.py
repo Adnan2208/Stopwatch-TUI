ec7c73fc32b4975c6e51d9f4908dd553634f0614
Adnan <adnan.c2208@gmail.com>
2026-05-09 20:14:40 +0530

    Added key bindings

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from textual.app import App
 from textual.widgets import Header,Footer
 
 class Stopwatch(App):
+    # BINDINGS, takes an array of tuples which represent the key bindings of the tui
+    BINDINGS = [
+        ("d","toggleDarkMode","This is a method that is used to toggle dark and light modes")
+    ]
+
     def compose(self):
         yield Header()
         yield Footer()
 
+    # You need to specify action_ while creating action function.
+    # You can omit the action_ prefix while mentioning them in the BINDINGS
+    def action_toggleDarkMode(self):
+        if(self.theme == "textual-dark"):
+            self.theme = ("textual-light")
+        else:
+            self.theme = ("textual-dark")
 
 if __name__ == "__main__":
     stopwatchObject = Stopwatch()
